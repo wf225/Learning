4e8fdf8c77c9bcce98eec7f34a757aa178b8cabb
WuFeng <wf225@126.com>
2016-08-04 09:07:17 +0800

    EC6 test

--- nodejs/vhd_test.js
+++ nodejs/vhd_test.js
@@ -2,6 +2,36 @@ var fs = require('fs');
 var util = require('util');
 var diskpart = require('./diskpart_helper.js');
 
+//类的定义
+class Animal {
+	//ES6中新型构造器
+    constructor(name) {
+        this.name = name;
+    }
+    //实例方法
+    sayName() {
+        console.log('My name is '+this.name);
+    }
+}
+//类的继承
+class Programmer extends Animal {
+    constructor(name) {
+    	//直接调用父类构造器进行初始化
+        super(name);
+    }
+    program() {
+        console.log("I'm coding...");
+    }
+}
+//测试我们的类
+var animal=new Animal('dummy'),
+wayou=new Programmer('wayou');
+animal.sayName();//输出 ‘My name is dummy’
+wayou.sayName();//输出 ‘My name is wayou’
+wayou.program();//输出 ‘I'm coding...’
+
+
+
 function create_vdisk_cmd(path, size) {
   fs.exists(path, function(exists) {
     var error;
@@ -20,6 +50,4 @@ function create_vdisk_cmd(path, size) {
   });  
 }
 
-
-
-create_vdisk_cmd('D:\\VHDs\\acadci-004.vhd', 1000);
+// create_vdisk_cmd('D:\\VHDs\\acadci-004.vhd', 1000);
